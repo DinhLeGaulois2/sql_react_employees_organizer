4e0c0077dd15ef4d9a9f161e1478c42b9c57b03b
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-28 04:31:59 -0400

    SCSS

---
+++ src/config/routes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+
+// Export the Routes
+module.exports = (
+
+  // The high level component is the Router component
+  <Router history={hashHistory}>
+  </Router>
+);
