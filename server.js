dcbc6e80fb98303c6eb1a1ab6798f84e748a1531
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-28 07:45:41 -0400

    Compiled Perfectly

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // error handling middleware
 app.use((err, req, res, next) => {
